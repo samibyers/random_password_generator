b05d2919b024b87e65b5d9ba0901387e5711fc19
Sami Byers <samtaylor.byers@gmail.com>
2021-09-23 18:42:06 -0500

    more added to JS

--- script.js
+++ script.js
@@ -8,27 +8,40 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
-function passwordCriteria() {
-//password length 
- {promt("How long do you want your password to be? (8-128 characters)")
- }
-//upper case, lower case
- {confirm("Do you want to include UPPERCASE letters?")
- }
- {confirm("Do you want to include lowercase letter?")
- }
-//numbers 
- {confirm("do you want to include numbers?")
- }
-//special characters
- {confirm("Do you want to include special characters?")
- }
-}
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+}
+
+
+
+button.onclick = function passwordCriteria() {
+//password length 
+var length = promt("How long do you want your password to be? (8-128 characters)")
+
+//upper case, lower case
+ var upperCase = confirm("Do you want to include UPPERCASE letters?")
+ if(upperCase === true) {
+      charsToInclude.push("uc")
+ }
+ var lowerCase = confirm("Do you want to include lowercase letter?")
+ if(lowerCase === true) {
+      charsToInclude.push("lc")
+ }
+//numbers 
+ var num = confirm("do you want to include numbers?")
+ if(num === true) {
+      charsToInclude.push("nu")
+ }
+//special characters
+ var spChar = confirm("Do you want to include special characters?")
+ if(spChar === true) {
+      charsToInclude.push("sp")
+ }
+return;
+};
+
+
+
 
 // var string = "abcdefghijklmnopqrstuvwxyz"; //to upper 
 //     var numeric = '0123456789';
